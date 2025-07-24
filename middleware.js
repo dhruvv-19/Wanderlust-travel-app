f833d21ffa2929d317a40ce78b92b40fef198e6d
Dhruv <dhruvyadav6707@gmail.com>
2025-07-24 15:20:10 +0530

    Add middleware for listing and review ownership checks

--- middleware.js
+++ middleware.js
@@ -1,3 +1,34 @@
+const Listing = require("./models/listing.js");
+const Review = require("./models/review.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+
+
+// middleware for schmea validation
+module.exports.validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    // console.log(result);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
+
+
+module.exports.validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+    // console.log(result);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
+
+
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()) {
         req.session.redirectUrl = req.originalUrl;
@@ -7,9 +38,32 @@ module.exports.isLoggedIn = (req, res, next) => {
     next();
 };
 
+
 module.exports.saveRedirectUrl = (req, res, next) => {
     if(req.session.redirectUrl){
         res.locals.redirectUrl = req.session.redirectUrl;
+    }
+    next();
+};
+
+
+module.exports.isOwner = async (req, res, next) => {
+    let {id} = req.params;
+    let listing = await Listing.findById(id);
+    if(!listing.owner.equals(res.locals.currUser._id)){
+        req.flash("error", "You're not the owner of this listing");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+};
+
+
+module.exports.isReviewCreatedBy = async (req, res, next) => {
+    let {id, reviewId} = req.params;
+    let review = await Review.findById(reviewId);
+    if(!review.createdBy.equals(res.locals.currUser._id)){
+        req.flash("error", "You didn't create this review");
+        return res.redirect(`/listings/${id}`);
     }
     next();
 };
