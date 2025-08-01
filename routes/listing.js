ffda797067c1e02b615b2d32562ff252aef28c39
Dhruv <dhruvyadav6707@gmail.com>
2025-08-01 15:15:29 +0530

    Connect controller and middleware to listing routes

--- routes/listing.js
+++ routes/listing.js
@@ -4,11 +4,17 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
 
+const multer  = require('multer');
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage });
+
+
 // index and create routes
 router.route("/")
     .get(wrapAsync(listingController.index)) // index route
     .post(
         isLoggedIn,
+        upload.single("listing[image]"),
         validateListing,
         wrapAsync(listingController.createNewListing) // create route
     );
@@ -22,6 +28,7 @@ router.route("/:id")
     .put(
         isLoggedIn,
         isOwner,
+        upload.single("listing[image]"),
         validateListing,
         wrapAsync(listingController.updateListing) // update route
     )
