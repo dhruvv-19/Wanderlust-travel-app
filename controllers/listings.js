0c6984cdf37139eb10a6358555c0e44c369bf4f9
Dhruv <dhruvyadav6707@gmail.com>
2025-08-01 15:11:49 +0530

    Add image upload and update logic in listing controller

--- controllers/listings.js
+++ controllers/listings.js
@@ -13,9 +13,15 @@ module.exports.renderNewForm = (req, res) => {
 
 // new listing creation controller
 module.exports.createNewListing = async (req, res, next) => {
+    let url = req.file.path;
+    let filename = req.file.filename;
+
     let listing = req.body.listing;
     const newListing = new Listing(listing);
     newListing.owner = req.user._id;
+
+    newListing.image = {url, filename};
+    
     await newListing.save();
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");
@@ -53,7 +59,15 @@ module.exports.renderEditFrom = async (req, res) => {
 // update listing controller
 module.exports.updateListing = async (req, res) => {
     let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+
+    if(typeof req.file !== "undefined"){
+        let url = req.file.path;
+        let filename = req.file.filename;
+        listing.image = {url, filename};
+        await listing.save();
+    }
+
     req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
 };
