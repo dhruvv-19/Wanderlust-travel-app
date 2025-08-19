84943a4d225affd62a48c07735e06add6f111015
Dhruv <dhruvyadav6707@gmail.com>
2025-08-19 13:20:13 +0530

    Add geometry field to Listing schema

--- models/listing.js
+++ models/listing.js
@@ -17,22 +17,33 @@ const listingSchema = new Schema({
     price: Number,
     location: String,
     country: String,
-    reviews: [{ 
-        type: Schema.Types.ObjectId, 
-        ref: "Review" 
+    reviews: [{
+        type: Schema.Types.ObjectId,
+        ref: "Review"
     }],
     owner: {
         type: Schema.Types.ObjectId,
         ref: "User",
     },
+    geometry: {
+        type: {
+            type: String, // Don't do `{ location: { type: String } }`
+            enum: ['Point'], // 'location.type' must be 'Point'
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    }
 });
 
-listingSchema.post("findOneAndDelete", async(listing) => {
-    if(listing){
-        await Review.deleteMany({_id: {$in: listing.reviews}});
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if (listing) {
+        await Review.deleteMany({ _id: { $in: listing.reviews } });
     }
 });
 
 
-const Listing = mongoose.model("Listing", listingSchema); 
+const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
