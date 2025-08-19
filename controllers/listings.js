dc198c3d5f57dac4e112fcc2423d0836618fe9ea
Dhruv <dhruvyadav6707@gmail.com>
2025-08-19 13:18:29 +0530

    Add geocoding logic to listings controller and save coordinates in geometry

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,4 +1,7 @@
 const Listing = require("../models/listing");
+const mbxGeocoding  = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 // index controller
 module.exports.index = async (req, res) => {
@@ -13,14 +16,21 @@ module.exports.renderNewForm = (req, res) => {
 
 // new listing creation controller
 module.exports.createNewListing = async (req, res, next) => {
+    let response = await geocodingClient.forwardGeocode({
+        query: req.body.listing.location, //store location in query
+        limit: 1,
+    })
+    .send()
+
     let url = req.file.path;
     let filename = req.file.filename;
 
     let listing = req.body.listing;
     const newListing = new Listing(listing);
     newListing.owner = req.user._id;
-
     newListing.image = {url, filename};
+
+    newListing.geometry = response.body.features[0].geometry;
     
     await newListing.save();
     req.flash("success", "New Listing Created!");
